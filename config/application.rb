3e47224c451ad900b58697c70bb261da246167dc
sawangpong muadphet <sawangpongm@gmail.com>
2014-03-04 18:38:56 +0700

    change font-awesome fa-calendar

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Shoperycms
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # devise post-install manual config
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end
